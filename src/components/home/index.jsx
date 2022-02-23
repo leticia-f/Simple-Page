6317e21bb6ac5e099f77b0c431db3c31e5357264
leticia-f <73148105+leticia-f@users.noreply.github.com>
2022-02-23 13:44:28 -0300

    imagens + texto início - 23.02

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,24 @@
 import homeBackground from '../imagens/yae-miko.jpg';
+import raidenshogun1 from '../imagens/raiden1.jpeg';
 import styled from "styled-components";
+import './home.css';
 
 const Home = () => {
     return (
         <>
-            <HomeImg src={homeBackground}/>
-            <h1 class="carousel-caption">Genshin</h1>
+            <body>
+                <HomeImg src={homeBackground} />
+                <br /><br /><br /><br /><br /><br />
+                <div class="container">
+                    <div class="col-5">
+                        <h1> Entre nesse Vasto Mundo Mágico de Aventura </h1> <br/>
+                        <h2> O vasto continente "Teyvat", onde incontáveis criaturas se reproduziram e se reuniram, contém todos os fenômenos do céu e da terra. Este é um mundo onde os sete deuses governam e se encontram. Sob o céu desconhecido, vocês são um par de gêmeos viajantes, vindos de um outro mundo. Separados por deuses estranhos, você foi selado e adormeceu. Ao acordar novamente, o cenário entre o céu e a terra mudou completamente...</h2>
+                    </div>
+                    <div class="col-5">
+                        <Raiden1 src={raidenshogun1}/>
+                    </div>
+                </div>
+            </body>
         </>
     );
 }
@@ -14,4 +27,8 @@ export default Home;
 
 const HomeImg = styled.img`
     height: 100%;
+`;
+
+const Raiden1 = styled.img`
+    width: 30%;
 `;
