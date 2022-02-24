15ea30008975d4e15e7f905ca63bb924f33b62f5
leticia-f <73148105+leticia-f@users.noreply.github.com>
2022-02-24 17:17:32 -0300

    colocando carousel na homescreen (com erros) - 24.02.22

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,6 +1,13 @@
 import homeBackground from '../imagens/yae-miko.jpg';
 import raidenshogun1 from '../imagens/raiden1.jpeg';
 import raidenshogun2 from '../imagens/raiden2.jpeg';
+
+import yaecarousel from '../imagens/carousel1yae.PNG';
+import zhongcarousel from '../imagens/carousel2zhongli.PNG';
+import yanfeicarousel from '../imagens/carousel3yanfei.PNG';
+import dionacarousel from '../imagens/carousel4diona.PNG';
+import diluccarousel from '../imagens/carousel5diluc.PNG';
+
 import styled from "styled-components";
 import './home.css';
 
@@ -8,6 +15,7 @@ const Home = () => {
     return (
         <>
             <body>
+
                 <HomeImg src={homeBackground} />
 
                 <br /><br /><br /><br /><br /><br />
@@ -30,6 +38,8 @@ const Home = () => {
                         </div>
                     </div>
 
+
+
                     <div class="ctnFlex">
                         <div class="op2">
                             <Raiden1 id="imagem" src={raidenshogun1} />
@@ -45,9 +55,50 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <br /><br /><br /><br />
+
+
+                    <div id="carouselcenter">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active d-flex justify-content-center" data-bs-interval="10000">
+                                    <Yae1 src={yaecarousel} />
+                                </div>
+                                <div class="carousel-item d-flex justify-content-center" data-bs-interval="5000">
+                                    <Zhong2 src={zhongcarousel} />
+                                </div>
+                                <div class="carousel-item d-flex justify-content-center" data-bs-interval="5000">
+                                    <Yanfei3 src={yanfeicarousel} />
+                                </div>
+                                <div class="carousel-item d-flex justify-content-center" data-bs-interval="5000">
+                                    <Diona4 src={dionacarousel} />
+                                </div>
+                                <div class="carousel-item d-flex justify-content-center" data-bs-interval="5000">
+                                    <Diluc5 src={diluccarousel} />
+                                </div>
+
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+
+
+
+                </div>
 
             </body>
         </>
@@ -67,4 +118,24 @@ const Raiden1 = styled.img`
 
 const Raiden2 = styled.img`
     width: 110%;
+`;
+
+const Yae1 = styled.img`
+    width: 80%;
+`;
+
+const Zhong2 = styled.img`
+    width: 80%;
+`;
+
+const Yanfei3 = styled.img`
+    width: 80%;
+`;
+
+const Diona4 = styled.img`
+    width: 80%;
+`;
+
+const Diluc5 = styled.img`
+    width: 80%;
 `;
