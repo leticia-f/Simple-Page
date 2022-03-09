f78632d7d3aface2a199a1902e7828da452f2b4b
leticia-f <leticia.fonseca.1106@gmail.com>
2022-03-09 19:28:42 -0300

    arrumando a homescreen (mais ou menos) - 09.03.22

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -20,44 +20,48 @@ const Home = () => {
 
                 <br /><br /><br /><br /><br /><br />
 
-                <div class="container">
-                    <div class="ctnFlex">
-                        <div class="op1">
-                            <div id="topo">
-                                <h1> Entre nesse Vasto Mundo Mágico de Aventura </h1>
-                                <h2> O vasto continente "Teyvat", onde incontáveis criaturas se reproduziram
-                                    e se reuniram, contém todos os fenômenos do céu e da terra. Este é um
-                                    mundo onde os sete deuses governam e se encontram. Sob o céu desconhecido,
-                                    vocês são um par de gêmeos viajantes, vindos de um outro mundo. Separados
-                                    por deuses estranhos, você foi selado e adormeceu. Ao acordar novamente,
-                                    o cenário entre o céu e a terra mudou completamente...</h2>
-                            </div>
-                        </div>
-                        <div class="op2">
-                            <Raiden2 id="imagem" src={raidenshogun2} />
+                <div class="container" id="menu">
+
+                    <div class="op1">
+                        <div id="topo">
+                            <h1> Entre nesse Vasto Mundo Mágico de Aventura </h1>
+                            <h2> O vasto continente "Teyvat", onde incontáveis criaturas se reproduziram
+                                e se reuniram, contém todos os fenômenos do céu e da terra. Este é um
+                                mundo onde os sete deuses governam e se encontram. Sob o céu desconhecido,
+                                vocês são um par de gêmeos viajantes, vindos de um outro mundo. Separados
+                                por deuses estranhos, você foi selado e adormeceu. Ao acordar novamente,
+                                o cenário entre o céu e a terra mudou completamente...</h2>
                         </div>
                     </div>
 
+                    <div class="op2">
+                        <Raiden2 id="imagem" src={raidenshogun2} />
+                    </div>
 
+                    <div class="op2">
+                        <Raiden1 id="imagem" src={raidenshogun1} />
+                    </div>
 
-                    <div class="ctnFlex">
-                        <div class="op2">
-                            <Raiden1 id="imagem" src={raidenshogun1} />
-                        </div>
-                        <div class="op1">
-                            <div id="topo">
-                                <h1> Disponível agora, versão 2.5: "O Florescer da Sakura" </h1>
-                                <h2> <b> [Nova Personagem] </b> Yae Miko <br />
-                                    <b> [Novo Evento] </b> Evento Temático da Versão, "Portal de Oferenda dos Três Reinos"<br />
-                                    <b> [Novo Enredo] </b> Atualização da Missão Lendária<br />
-                                    <b> [Nova Arma] </b> Prova de Kagura<br />
-                                    <b> [Novo Monstro] </b> Narukami, Comandante da Catástrofe</h2>
-                            </div>
+                    <div class="op1">
+                        <div id="topo">
+                            <h1> Disponível agora, versão 2.5: "O Florescer da Sakura" </h1>
+                            <h2> <b> [Nova Personagem] </b> Yae Miko <br />
+                                <b> [Novo Evento] </b> Evento Temático da Versão, "Portal de Oferenda dos Três Reinos"<br />
+                                <b> [Novo Enredo] </b> Atualização da Missão Lendária<br />
+                                <b> [Nova Arma] </b> Prova de Kagura<br />
+                                <b> [Novo Monstro] </b> Narukami, Comandante da Catástrofe</h2>
                         </div>
                     </div>
+
+                </div>
+
+
+                <div class="container" id="carrossel">
+
+                    <div id="espacos"><p></p></div>
 
                     <div id="carouselcenter">
-                        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                        <div id="carouselExampleIndicators" class="carousel carousel-fade" data-bs-ride="carousel">
                             <div class="carousel-indicators">
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -81,7 +85,6 @@ const Home = () => {
                                 <div class="carousel-item justify-content-center" data-bs-interval="5000">
                                     <Diluc5 src={diluccarousel} />
                                 </div>
-
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -94,6 +97,7 @@ const Home = () => {
                         </div>
                     </div>
 
+                    <div id="espacos"><p></p></div>
 
                 </div>
 
@@ -110,29 +114,29 @@ const HomeImg = styled.img`
 `;
 
 const Raiden1 = styled.img`
-    width: 110%;
+    width: 100%;
 `;
 
 const Raiden2 = styled.img`
-    width: 110%;
+    width: 100%;
 `;
 
 const Yae1 = styled.img`
-    width: 80%;
+    width: 100%;
 `;
 
 const Zhong2 = styled.img`
-    width: 80%;
+    width: 100%;
 `;
 
 const Yanfei3 = styled.img`
-    width: 80%;
+    width: 100%;
 `;
 
 const Diona4 = styled.img`
-    width: 80%;
+    width: 100%;
 `;
 
 const Diluc5 = styled.img`
-    width: 80%;
+    width: 100%;
 `;
